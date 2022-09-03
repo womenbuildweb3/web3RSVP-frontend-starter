e928b5a3a0b1aab6ab992b8d4c9261debe199968
Alex <alkhateeb.alex@hotmail.com>
2022-09-03 11:03:42 +0300

    added Wagmi and RainbowKit

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,13 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import Navmenu from "./Navmenu";
 
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect } from "wagmi";
+
 export default function Navbar() {
   const [mounted, setMounted] = useState(false);
+  const { data: account } = useAccount();
+  const { disconnect } = useDisconnect();
 
   useEffect(() => {
     setMounted(true);
@@ -24,10 +29,15 @@ export default function Navbar() {
             </div>
             <div className="ml-10 space-x-4 flex items-center">
               <Link href="/create-event">
-                <a className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 border border-indigo-100 hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <a className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 border-indigo-100 hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                   Create Event
                 </a>
               </Link>
+              {account ? (
+                <Navmenu account={account} disconnect={() => disconnect()} />
+              ) : (
+                <ConnectButton />
+              )}
             </div>
           </div>
         </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,36 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
+const { chains, provider } = configureChains(
+  [chain.polygon],
+  [alchemyProvider({ alchemyId }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "web3rsvp",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} coolMode>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
